188101cf83c4489df10bff04c8c978829f6864d5
Meham85 <codedtp@gmial.com>
2019-06-05 21:11:09 +0200

    final version

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.*;
 import java.lang.*;
 public class ShapeCollector  {
-
+    Shape Figure;
     public ArrayList<Shape>  figureArrayList = new ArrayList<Shape>();
 
     public void addFigure(Shape shape) {
@@ -14,23 +14,28 @@ public class ShapeCollector  {
 
 
     public void removeFigure(Shape shape) {
-        if (figureArrayList.contains(shape) == false)
-            throw new IllegalArgumentException("Invalid Shape name");
-        figureArrayList.remove(shape);
+        if (figureArrayList.contains(shape) == false) {
+            System.out.println("This figure is not out array element");
+        } else {
+            figureArrayList.remove(shape);
+        }
     }
     public Shape getFigure(int index) {
-        if (index <0 || index > figureArrayList.size())
-            throw new IllegalArgumentException("Invalid index specyfication");
-        return figureArrayList.get(index);
-
+        if (index < 0 || index > figureArrayList.size()) {
+            System.out.println("This figure is not out array element");
+        } else {
+            Figure = figureArrayList.get(index);
+        }
+        return Figure;
     }
-   /*  public Shape showFigures() {
-        for (int i =0; i< figureArrayList.size(); i++) {
-            return Shape temportaryFigure = figureArrayList.get(i);
-        }
-        for( Shape figures: figureArrayList) {
-            System.out.println("name " + figures.getShapeName() + "field " + figures.getField());
-        }
-        return Shape; */
+
+   public void showFigures() {
+       for (int i = 0; i < figureArrayList.size(); i++) {
+           Shape temportaryFigure = figureArrayList.get(i);
+
+           System.out.println("name " + temportaryFigure.getShapeName() + " field " + temportaryFigure.getField());
+       }
+   }
+
     }
 
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -53,17 +53,7 @@ public class ShapeCollectorTestSuite {
         int removeSize = shapeCollector.figureArrayList.size();
         Assert.assertEquals(afterSize, removeSize +1);
     }
-    @Test(expected = IllegalArgumentException.class)
-    public void removeFigureWrong() {
-        Circle testCircle = new Circle("circle1", 10.00);
 
-        ShapeCollector shapeCollector = new ShapeCollector();
-        /* shapeCollector.addFigure(testCircle) ; */
-        int afterSize = shapeCollector.figureArrayList.size();
-        shapeCollector.removeFigure(testCircle) ;
-        int removeSize = shapeCollector.figureArrayList.size();
-        Assert.assertEquals(afterSize, removeSize +1);
-    }
 
     @Test
     public void getFigure() {
@@ -80,5 +70,19 @@ public class ShapeCollectorTestSuite {
         Shape gettingFigure = shapeCollector.getFigure(3);
         Assert.assertEquals("circle4", gettingFigure.getShapeName());
     }
+    @Test
+    public void showFigures() {
+        Circle testCircle1 = new Circle("circle1", 10.00);
+        Square testSquare2 = new Square("square2", 15.00);
+        Triangle testTriangle3 = new Triangle("triangle3", 20.00);
+        Circle testCircle4 = new Circle("circle4", 25.00);
 
+        ShapeCollector shapeCollector = new ShapeCollector();
+        shapeCollector.addFigure(testCircle1) ;
+        shapeCollector.addFigure(testSquare2) ;
+        shapeCollector.addFigure(testTriangle3) ;
+        shapeCollector.addFigure(testCircle4) ;
+        shapeCollector.showFigures();
+
+    }
 }
