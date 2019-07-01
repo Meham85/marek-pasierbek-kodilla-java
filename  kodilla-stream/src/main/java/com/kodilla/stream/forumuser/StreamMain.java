645234d6938d39df2f66e2073b04673fbe25b60c
Meham85 <codedtp@gmial.com>
2019-07-01 22:54:55 +0200

    poprawiona data

---  kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++  kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -1,4 +1,5 @@
 package com.kodilla.stream.forumuser;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,8 +8,8 @@ import com.kodilla.stream.forumuser.ForumUser;
 public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
-        Date currentDate = new Date();
-        int currentYear = currentDate.getYear() +1900;
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear() ;
         System.out.println(currentYear);
         System.out.println(currentDate);
         Map<Integer, ForumUser> NewForum = forum.getUserList().stream()
