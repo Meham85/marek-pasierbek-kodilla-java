08fff9138049a0bceefb5234bd63093865cb7d6a
Meham85 <codedtp@gmial.com>
2019-09-23 10:14:46 +0200

    moduł 15 Strategy

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,30 @@
+
+package com.kodilla.patterns.strategy.social;
+
+abstract class User {
+    private final String username;
+    private SocialPublisher socialPublisher;
+
+    User(final String username, final SocialPublisher socialPublisher) {
+        if (socialPublisher == null || username == null) {
+            throw new IllegalArgumentException();
+        }
+        this.username = username;
+        this.socialPublisher = socialPublisher;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String sharePost() {
+        return socialPublisher.share();
+    }
+
+    void setSocialPublisher(final SocialPublisher socialPublisher) {
+        if (socialPublisher == null) {
+            throw new IllegalArgumentException();
+        }
+        this.socialPublisher = socialPublisher;
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/YGeneration.java
@@ -0,0 +1,8 @@
+
+package com.kodilla.patterns.strategy.social;
+
+class YGeneration extends User {
+    YGeneration(final String username) {
+        super(username, new TwitterPublisher());
+    }
+}
